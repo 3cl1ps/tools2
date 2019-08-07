1af9b3f5639d9e4a1039e3c31cd851bd239be070
eclips <lagane.thomas@gmail.com>
2019-08-07 14:25:09 +0200

    i

--- checkiguanaio.sh
+++ checkiguanaio.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # (c) fullmoon
 #myip=$(curl -s4 checkip.amazonaws.com)
-myip=$(hostname -I)
-echo $myip
+myip=$(hostname -I | sed 's/.$//')
 establishedconnections=$( ss -a | grep ":17776" | grep "ESTAB" | awk '{print $5 " "  $6}' )
 
 #echo $"$establishedconnections"
