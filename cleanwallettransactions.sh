893dc28ec73f7ced0618e4fa59bd670fa80d805f
3cl1ps <lagane.thomas@gmail.com>
2019-08-18 09:59:43 +0200

    Create cleanwallettransactions.sh

---
+++ cleanwallettransactions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# @author webworker01
+
+source /home/eclips/tools2/coinlist
+source /home/eclips/tools2/config
+
+dt=$(date '+%Y-%m-%d %H:%M:%S');
+
+cleanerremoved=$($komodocli cleanwallettransactions | jq -r .removed_transactions)
+if (( cleanerremoved > 0 )); then
+    echo "$dt [cleanwallettransactions] KMD - Removed $cleanerremoved transactions"
+fi
+
+if (( thirdpartycoins < 1 )); then
+    for coins in "${coinlist[@]}"; do
+        coin=($coins)
+        if [[ ! ${ignoreacs[*]} =~ ${coin[0]} ]]; then
+            #echo ${coin[0]}
+            cleanerremoved=$($komodocli -ac_name=${coin[0]} cleanwallettransactions | jq -r .removed_transactions)
+            if (( cleanerremoved > 0 )); then
+                echo "$dt [cleanwallettransactions] ${coin[0]} - Removed $cleanerremoved transactions"
+            fi
+        fi
+    done
+fi
