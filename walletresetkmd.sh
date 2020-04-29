4754afdfce24d71fe0d289d8906f953cec19b1cc
3cl1ps <lagane.thomas@gmail.com>
2020-04-29 13:38:01 +0200

    Update walletresetkmd.sh

--- walletresetkmd.sh
+++ walletresetkmd.sh
@@ -3,7 +3,7 @@ cd "${BASH_SOURCE%/*}" || exit
 
 coin="KMD"
 daemon="/home/eclips/komodo/src/komodod -pubkey=${PUBKEY} -minrelaytxfee=0.000035 -opretmintxfee=0.004"
-daemon_process_regex="komodod.*\-pubkey"
+daemon_process_regex="komodod.*\-pubkey.*\-minrelaytxfee"
 cli="komodo-cli"
 wallet_file="${HOME}/.komodo/wallet.dat"
 nn_address=$KMDADDRESS
